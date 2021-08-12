48b42f0dfa5580654076611285fc2b76f897a65a
Juliette Monsel <j_4321@protonmail.com>
2021-08-12 13:34:54 +0200

    Improve PEP8 compliance

--- tkPDFViewer/tkPDFViewer.py
+++ tkPDFViewer/tkPDFViewer.py
@@ -1,5 +1,5 @@
 try:
-    from tkinter import*
+    from tkinter import *
     import fitz
     from tkinter.ttk import Progressbar
     from threading import Thread
@@ -11,27 +11,30 @@ class ShowPdf():
 
     img_object_li = []
 
-    def pdf_view(self,master,width=1200,height=600,pdf_location="",bar=True,load="after"):
+    def pdf_view(self, master, width=1200, height=600, pdf_location="", bar=True, load="after"):
 
-        self.frame = Frame(master,width= width,height= height,bg="white")
+        self.frame = Frame(master, width=width, height=height, bg="white")
 
-        scroll_y = Scrollbar(self.frame,orient="vertical")
-        scroll_x = Scrollbar(self.frame,orient="horizontal")
+        scroll_y = Scrollbar(self.frame, orient="vertical")
+        scroll_x = Scrollbar(self.frame, orient="horizontal")
 
-        scroll_x.pack(fill="x",side="bottom")
-        scroll_y.pack(fill="y",side="right")
+        scroll_x.pack(fill="x", side="bottom")
+        scroll_y.pack(fill="y", side="right")
 
         percentage_view = 0
         percentage_load = StringVar()
 
-        if bar==True and load=="after":
+        if bar and load == "after":
             self.display_msg = Label(textvariable=percentage_load)
             self.display_msg.pack(pady=10)
 
-            loading = Progressbar(self.frame,orient= HORIZONTAL,length=100,mode='determinate')
-            loading.pack(side = TOP,fill=X)
+            loading = Progressbar(self.frame, orient=HORIZONTAL,
+                                  length=100, mode='determinate')
+            loading.pack(side=TOP, fill=X)
 
-        self.text = Text(self.frame,yscrollcommand=scroll_y.set,xscrollcommand= scroll_x.set,width= width,height= height)
+        self.text = Text(self.frame, yscrollcommand=scroll_y.set,
+                         xscrollcommand=scroll_x.set,
+                         width=width, height=height)
         self.text.pack(side="left")
 
         scroll_x.config(command=self.text.xview)
@@ -44,30 +47,30 @@ class ShowPdf():
 
             for page in open_pdf:
                 pix = page.getPixmap()
-                pix1 = fitz.Pixmap(pix,0) if pix.alpha else pix
+                pix1 = fitz.Pixmap(pix, 0) if pix.alpha else pix
                 img = pix1.getImageData("ppm")
-                timg = PhotoImage(data = img)
+                timg = PhotoImage(data=img)
                 self.img_object_li.append(timg)
-                if bar==True and load=="after":
+                if bar and load == "after":
                     precentage_dicide = precentage_dicide + 1
                     percentage_view = (float(precentage_dicide)/float(len(open_pdf))*float(100))
                     loading['value'] = percentage_view
                     percentage_load.set(f"Please wait!, your pdf is loading {int(math.floor(percentage_view))}%")
-            if bar==True and load=="after":
+            if bar and load == "after":
                 loading.pack_forget()
                 self.display_msg.pack_forget()
 
             for i in self.img_object_li:
-                self.text.image_create(END,image=i)
-                self.text.insert(END,"\n\n")
+                self.text.image_create(END, image=i)
+                self.text.insert(END, "\n\n")
             self.text.configure(state="disabled")
 
         def start_pack():
             t1 = Thread(target=add_img)
             t1.start()
 
-        if load=="after":
-            master.after(250,start_pack)
+        if load == "after":
+            master.after(250, start_pack)
         else:
             start_pack()
 
@@ -79,9 +82,9 @@ class ShowPdf():
 def main():
     root = Tk()
     root.geometry("700x780")
-    d = ShowPdf().pdf_view(root,pdf_location=r"D:\DELL\Documents\Encyclopedia GUI.pdf",width=50,height=200)
+    d = ShowPdf().pdf_view(root, pdf_location=r"D:\DELL\Documents\Encyclopedia GUI.pdf", width=50, height=200)
     d.pack()
     root.mainloop()
 
-if __name__ == '__main__':
+if __name__  ==  '__main__':
     main()
