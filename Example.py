83df848f09626c54079403e268e0db4a25bda6f6
Juliette Monsel <j_4321@protonmail.com>
2021-08-12 14:08:19 +0200

    Move ShowPdf to __init__.py and add __main__.py
    The package contained a single meaningful file, tkPDFViewer.py which
    consisted of the class ShowPdf and a main() function to execute an example.
    To avoid having to import tkPDFViewer.tkPDFViewer.ShowPdf each time, it makes
    more sense to move ShowPdf and main() to  __init__.py while the `if __name__ == "__main__"`
    block should be in the __main__.py. Changed main() so that the filepath is not hard coded
    but is given in argument through the command line: `python -m tkPDFViewer <pdf file path>`

--- Example.py
+++ Example.py
@@ -1,4 +1,4 @@
-from tkPDFViewer import tkPDFViewer as pdf
+import tkPDFViewer as pdf
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
@@ -7,10 +7,11 @@ root = Tk()
 file = askopenfilename(title="Open PDF", defaultextension=".pdf",
                        filetypes=[("PDF", "*.pdf"), ("All files", "*")])
 
-#create object like this.
-viewer = pdf.ShowPdf()
-#Add your pdf location and width and height.
-pdf_frame = viewer.pdf_view(root, pdf_location=file,
-                            width=50,height=100)
-pdf_frame.pack()
-root.mainloop()
+if file:
+    #create object like this.
+    viewer = pdf.ShowPdf()
+    #Add your pdf location and width and height.
+    pdf_frame = viewer.pdf_view(root, pdf_location=file,
+                                width=50,height=100)
+    pdf_frame.pack()
+    root.mainloop()
--- tkPDFViewer/__init__.py
+++ tkPDFViewer/__init__.py
@@ -1,1 +1,93 @@
+try:
+    import tkinter as tk
+    import fitz
+    from tkinter import ttk
+    from threading import Thread
+    import math
+    import sys
+except Exception as e:
+    print(f"This error occured while importing neccesary modules or library {e}")
 
+class ShowPdf():
+
+    def __init__(self):
+        self.img_object_li = []
+
+    def pdf_view(self, master, width=1200, height=600, pdf_location="", bar=True, load="after"):
+
+        self.frame = tk.Frame(master, width=width, height=height, bg="white")
+
+        scroll_y = ttk.Scrollbar(self.frame, orient=tk.VERTICAL)
+        scroll_x = ttk.Scrollbar(self.frame, orient=tk.HORIZONTAL)
+
+        scroll_x.pack(fill=tk.X, side=tk.BOTTOM)
+        scroll_y.pack(fill=tk.Y, side=tk.RIGHT)
+
+        percentage_view = 0
+        percentage_load = tk.StringVar(master)
+
+        if bar and load == "after":
+            self.display_msg = ttk.Label(textvariable=percentage_load)
+            self.display_msg.pack(pady=10)
+
+            loading = ttk.Progressbar(self.frame, orient=tk.HORIZONTAL,
+                                      length=100, mode='determinate')
+            loading.pack(side=tk.TOP, fill=tk.X)
+
+        self.text = tk.Text(self.frame, yscrollcommand=scroll_y.set,
+                            xscrollcommand=scroll_x.set,
+                            width=width, height=height)
+        self.text.pack(side="left")
+
+        scroll_x.config(command=self.text.xview)
+        scroll_y.config(command=self.text.yview)
+
+        def add_img():
+            precentage_dicide = 0
+            open_pdf = fitz.open(pdf_location)
+
+            for page in open_pdf:
+                pix = page.getPixmap()
+                pix1 = fitz.Pixmap(pix, 0) if pix.alpha else pix
+                img = pix1.getImageData("ppm")
+                timg = tk.PhotoImage(data=img)
+                self.img_object_li.append(timg)
+                if bar and load == "after":
+                    precentage_dicide = precentage_dicide + 1
+                    percentage_view = (float(precentage_dicide)/float(len(open_pdf))*float(100))
+                    loading['value'] = percentage_view
+                    percentage_load.set(f"Please wait!, your pdf is loading {int(math.floor(percentage_view))}%")
+            if bar and load == "after":
+                loading.pack_forget()
+                self.display_msg.pack_forget()
+
+            for i in self.img_object_li:
+                self.text.image_create(tk.END, image=i)
+                self.text.insert(tk.END, "\n\n")
+            self.text.configure(state="disabled")
+
+        def start_pack():
+            t1 = Thread(target=add_img)
+            t1.start()
+
+        if load == "after":
+            master.after(250, start_pack)
+        else:
+            start_pack()
+
+        return self.frame
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Missing argument: path of pdf to open")
+        return
+
+    root = tk.Tk()
+    #create object like this.
+    viewer = ShowPdf()
+    #Add your pdf location and width and height.
+    pdf_frame = viewer.pdf_view(root, pdf_location=sys.argv[1],
+                                width=50, height=100)
+    pdf_frame.pack()
+    root.mainloop()
