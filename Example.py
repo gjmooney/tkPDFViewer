5d648b07405e0c7799fa2d44d7d276e36ff539e8
Juliette Monsel <j_4321@protonmail.com>
2021-08-12 15:02:44 +0200

    Add open button in example

--- Example.py
+++ Example.py
@@ -1,17 +1,22 @@
 import tkPDFViewer as pdf
-from tkinter import Tk
+from tkinter import Tk, Button
 from tkinter.filedialog import askopenfilename
+
+
+def open_pdf():
+    file = askopenfilename(title="Open PDF", defaultextension=".pdf",
+                           filetypes=[("PDF", "*.pdf"), ("All files", "*")])
+    if file:
+        pdf_frame = viewer.pdf_view(pdf_location=file,
+                                    width=50, height=100)
+        pdf_frame.pack(fill="both", expand=True)
 
 root = Tk()
 
-file = askopenfilename(title="Open PDF", defaultextension=".pdf",
-                       filetypes=[("PDF", "*.pdf"), ("All files", "*")])
+#create object like this.
+viewer = pdf.ShowPdf(root)
+viewer2 = pdf.ShowPdf(root)
 
-if file:
-    #create object like this.
-    viewer = pdf.ShowPdf(root)
-    #Add your pdf location and width and height.
-    pdf_frame = viewer.pdf_view(pdf_location=file,
-                                width=50,height=100)
-    pdf_frame.pack()
-    root.mainloop()
+Button(root, text="Open pdf", command=open_pdf).pack()
+
+root.mainloop()
